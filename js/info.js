b8c72fb87d12a5cecedd99e1a5aef95d0c8cbca2
Jaime García <jgarcia@digitalrising.com.gt>
2019-02-13 10:04:31 -0600

    Fuentes Peek

---
+++ js/info.js
@@ -0,0 +1,24 @@
+﻿function MuestraDivInsert1() {
+    $('#divinsert1').toggle();
+
+}
+
+function OcultaDivInsert1() {
+    $('#divinsert1').toggle();
+
+}
+
+function MuestraDivInsert2() {
+    $('#divinsert2').toggle();
+ 
+}
+
+function MuestraDivInsert3() {
+    $('#divinsert3').toggle();
+
+}
+
+function MuestraDivInsert4() {
+    $('#divinsert4').toggle();
+  
+}
